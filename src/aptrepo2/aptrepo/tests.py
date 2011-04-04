01ff3f0d0bf0d2b5853d8320f89c02b3085db177
Bijan Vakili <bijanvakili@server.fake>
2011-04-03 20:50:40 -0400

    - First working version with package upload and hashing - Added first unit test

--- src/aptrepo2/aptrepo/tests.py
+++ src/aptrepo2/aptrepo/tests.py
@@ -1,23 +1,40 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+Unit tests for apt repo
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
+import os
+import tempfile
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from aptrepo2.settings import TEST_DATA_ROOT, APTREPO_FILESTORE_ROOT
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class PackageUploadTest(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
+    def setUp(self):
+        # remove all previously uploaded Debian files
+        os.system('rm {0}/*.deb'.format(APTREPO_FILESTORE_ROOT))
 
+    def _create_package(self, src_root_dir, pkg_filename):
+        ret = os.system('dpkg --build {0} {1}'.format(src_root_dir, pkg_filename))
+        return (ret >> 16) == 0
+    
+    def test_package_upload(self):
+        """ test a simple package upload """
+        pkg_filename = None
+        try:
+            c = Client()
+            pkg_fh, pkg_filename = tempfile.mkstemp(suffix='.deb', prefix='mypackage')
+            os.close(pkg_fh)
+            self.assertTrue(self._create_package(
+                os.path.join(TEST_DATA_ROOT,'test-package'), 
+                pkg_filename))
+
+            # upload the file            
+            with open(pkg_filename) as f:
+                response = c.post('/aptrepo/package/upload', {'attachment' : f})
+                print response.content
+                self.failUnlessEqual(response.status_code, 302)
+                
+        finally:
+            if pkg_filename is not None:
+                os.remove(pkg_filename)
--- src/aptrepo2/settings.py
+++ src/aptrepo2/settings.py
@@ -11,7 +11,9 @@ if 'APTREPO_ROOT' in os.environ:
     APTREPO_ROOT = os.environ['APTREPO_ROOT']
 APTREPO_VAR_ROOT = os.path.join(APTREPO_ROOT, 'var')
 APTREPO_SHARE_ROOT =  os.path.join(APTREPO_ROOT, 'share')
+APTREPO_FILESTORE_ROOT = os.path.join(APTREPO_VAR_ROOT,'files')
 
+TEST_DATA_ROOT = os.path.join(APTREPO_ROOT, 'test/data')
 
 ADMINS = (
     ('Build Administrator', 'buildadmins@oanda.com'),
