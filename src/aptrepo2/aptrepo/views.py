58589644c6f505d842fb986561deb9af45fcbe96
Bijan Vakili <bvakili@oanda.com>
2011-04-10 12:24:01 -0400

    Got python-debian module to work.  No need for dpkg CLI commands

--- src/aptrepo2/aptrepo/views.py
+++ src/aptrepo2/aptrepo/views.py
@@ -2,12 +2,11 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from aptrepo2.settings import APTREPO_FILESTORE_ROOT
+from debian_bundle import debfile
 import models
 import os
 import hashlib
-import re
-import string
-import subprocess
+
 
 fs = FileSystemStorage(location=APTREPO_FILESTORE_ROOT)
 HASH_BLOCK_MULTIPLE = 128
@@ -29,27 +28,6 @@ def _verify_debian(deb_file):
     (_, ext) = os.path.splitext(deb_file.name)
     if ext != '.deb':
         raise AptRepoException('Invalid extension: {0}'.format(ext))
-    
-    
-def _get_debian_control_data(deb_filename):
-    """ Retrieve the field name """
-    
-    # retrieve the control information
-    dpkg_cmd = 'dpkg-deb --info {0} control'.format(deb_filename)
-    dpkg_proc = subprocess.Popen(args=['dpkg-deb', '--info', deb_filename, 'control'],
-                                 stdout=subprocess.PIPE)
-    dpkg_proc.wait()
-    if dpkg_proc.returncode != 0:
-        raise AptRepoException("Command failed: " + dpkg_cmd)
-    control_data = dpkg_proc.stdout.readlines()
-    
-    # convert the control data to a dictionary
-    control = {}
-    for line in control_data:
-        if not re.match('^\s', line):
-            (k,v) = string.split(line, ':')
-            control[k.strip()] = v.strip()
-    return control
     
     
 def _hash_digest_file(hashfunc, filename):
@@ -78,7 +56,8 @@ def upload_file(request):
         package = models.Package(filepath=new_file_path)
         
         # extract control file information
-        control = _get_debian_control_data(new_file_path)
+        deb = debfile.DebFile(new_file_path)
+        control = deb.debcontrol()
         package.package_name = control['Package']
         package.architecture = control['Architecture']
         package.version = control['Version']
