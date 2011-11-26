d4b4d796b2630beac6bb131cf32026cef7e58c07
Bijan Vakili <bvakili@oanda.com>
2011-11-25 23:26:03 -0500

    - Added decorators to web and API views for handling exceptions to avoid duplicated try/catch statements - Added _error_BadHTTPMethodException to web view - Modified authorization exception to include action and user in exception message - Modified authroziation exceptions to only log at the INFO level instead of full stack trace at the ERROR level

--- src/server/aptrepo/views/webpages/pages.py
+++ src/server/aptrepo/views/webpages/pages.py
@@ -1,3 +1,4 @@
+import httplib
 import logging
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
@@ -8,7 +9,7 @@ from django import forms
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from server.aptrepo import models
-from server.aptrepo.util import AptRepoException, constants
+from server.aptrepo.util import AptRepoException, AuthorizationException, constants
 from server.aptrepo.views import get_repository_controller
 
 
@@ -18,42 +19,78 @@ class UploadPackageForm(forms.Form):
     """
     file = forms.FileField()
     section = forms.ModelChoiceField(queryset=models.Section.objects.all())
-    
 
+class BadHTTPMethodException(AptRepoException):
+    """
+    Exception for requests with invalid HTTP methods (GET versus PUTS)  
+    """
+    
+    def __init__(self, request=None):
+        message = "Invalid HTTP method"
+        if request:
+            message = message + " (" + request.method + ")"
+        super(BadHTTPMethodException, self).__init__(message)
+
+def handle_exception(request_handler_func):
+    """
+    Decorator function for handling exceptions and converting them
+    to the appropriate response for the web client
+    """
+    def wrapper_handler(*args, **kwargs):
+        logger = logging.getLogger(settings.DEFAULT_LOGGER)
+
+        try:
+            return request_handler_func(*args, **kwargs)
+        except BadHTTPMethodException as e:
+            logger.info(e)
+            return HttpResponse(str(e), 
+                                content_type='text/plain', 
+                                status=httplib.FORBIDDEN)
+        except AuthorizationException as e:
+            logger.info(e)
+            return HttpResponse(str(e), 
+                                content_type='text/plain', 
+                                status=httplib.UNAUTHORIZED)
+        except Exception as e:
+            logger.exception(e)
+            if settings.DEBUG:
+                raise
+            else:
+                return HttpResponse(str(e), 
+                                    content_type='text/plain', 
+                                    status=httplib.INTERNAL_SERVER_ERROR)
+    
+    return wrapper_handler
+
+@handle_exception
 def gpg_public_key(request):
     """
     Retrieves the GPG public key
     """
-    try:
-        if request.method != 'GET':
-            raise AptRepoException('Invalid HTTP method')
-        
-        repository = get_repository_controller(request=request)
-        return HttpResponse(repository.get_gpg_public_key())
+    if request.method != 'GET':
+        raise BadHTTPMethodException(request)
+    
+    repository = get_repository_controller(request=request)
+    return HttpResponse(repository.get_gpg_public_key())
 
-    except Exception as e:
-        return _error_response(e)
-
+@handle_exception
 def packages(request):
     """ 
     Handles package requests (no UI) 
     """
-    try:
-        if request.method == 'POST':
-            return packages_post(request)
-        
-        elif request.method == 'GET':
-            """ Get method at root will list all packages """
-            package_list = models.Package.objects.all().order_by('package_name')
-            return render_to_response('aptrepo/packages_index.html', {'packages': package_list})
-                     
-        else:
-            raise AptRepoException('Invalid HTTP method')
+    if request.method == 'POST':
+        return packages_post(request)
     
-    except Exception as e:
-        return _error_response(e)
+    elif request.method == 'GET':
+        """ Get method at root will list all packages """
+        package_list = models.Package.objects.all().order_by('package_name')
+        return render_to_response('aptrepo/packages_index.html', {'packages': package_list})
+                 
+    else:
+        raise BadHTTPMethodException(request)
     
 
+@handle_exception
 @login_required
 def packages_post(request):
     """ POST requests will upload a package and create a new record """
@@ -79,119 +116,106 @@ def remove_success(request):
     return HttpResponse("Package successfully removed.")
 
 
+@handle_exception
 @login_required
 @csrf_protect
 def upload_file(request):
     """ 
     Provides a form to upload packages
     """
-    try:
-        if request.method == 'POST':
-            form = UploadPackageForm(request.POST, request.FILES)
-            if form.is_valid():
-                section = form.cleaned_data['section']
-                return _handle_uploaded_file(request,
-                                             section.distribution.name,
-                                             section.name, 
-                                             request.FILES['file'])
+    if request.method == 'POST':
+        form = UploadPackageForm(request.POST, request.FILES)
+        if form.is_valid():
+            section = form.cleaned_data['section']
+            return _handle_uploaded_file(request,
+                                         section.distribution.name,
+                                         section.name, 
+                                         request.FILES['file'])
 
-        elif request.method == 'GET':
-            form = UploadPackageForm()
-            
-        else:
-            raise AptRepoException('Invalid HTTP method')
-
-        return render_to_response('aptrepo/upload_package.html', {'form':form}, 
-                                  context_instance=RequestContext(request))
+    elif request.method == 'GET':
+        form = UploadPackageForm()
         
-    except Exception as e:
-        return _error_response(e)
+    else:
+        raise BadHTTPMethodException(request)
+
+    return render_to_response('aptrepo/upload_package.html', {'form':form}, 
+                              context_instance=RequestContext(request))
     
     
+@handle_exception
 @login_required
 def delete_package_instance(request):
     """
     Basic HTTP POST call to remove a package instance
     """
-    try:
-        if request.method != 'POST':
-            raise AptRepoException('Invalid HTTP method')
+    if request.method != 'POST':
+        raise BadHTTPMethodException(request)
+    
+    # extract the package instance identifier
+    package_instance_id = 0
+    if 'package_instance' in request.POST:
+        package_instance_id = request.POST['package_instance']
+    else:
+        package_instance = models.PackageInstance.objects.get(
+            section__distribution__name=request.POST['distribution'],
+            section__name=request.POST['section'],
+            package__package_name=request.POST['name'],
+            package__architecture=request.POST['architecture'],
+            package__version=request.POST['version'])
+        if package_instance is None:
+            raise AptRepoException("Package instance not found for removal")
+        package_instance_id = package_instance.id
         
-        # extract the package instance identifier
-        package_instance_id = 0
-        if 'package_instance' in request.POST:
-            package_instance_id = request.POST['package_instance']
-        else:
-            package_instance = models.PackageInstance.objects.get(
-                section__distribution__name=request.POST['distribution'],
-                section__name=request.POST['section'],
-                package__package_name=request.POST['name'],
-                package__architecture=request.POST['architecture'],
-                package__version=request.POST['version'])
-            if package_instance is None:
-                raise AptRepoException("Package instance not found for removal")
-            package_instance_id = package_instance.id
-            
-        return _handle_remove_package(request, package_instance_id)
-        
-    except Exception as e:
-        return _error_response(e)
+    return _handle_remove_package(request, package_instance_id)
 
-
+@handle_exception
 def package_list(request, distribution, section, architecture, extension=None):
     """
     Retrieve a package list
     """
-    try:
-        if request.method != 'GET':
-            raise AptRepoException('Invalid HTTP method')
+    if request.method != 'GET':
+        raise BadHTTPMethodException(request)
 
-        # NOTE: caching is not employed for package list
-                  
-        is_compressed = False
-        if extension == constants.GZIP_EXTENSION:
-            is_compressed = True
-        elif extension != '':
-            return HttpResponse(status_code=404)
-            
-        mimetype = 'text/plain'
-        if is_compressed:
-            mimetype = 'application/gzip'
-        repository = get_repository_controller(request=request)
-        response = HttpResponse(mimetype=mimetype)
-        response.content = repository.get_packages(distribution, section, architecture,
-                                                   is_compressed)
-        response['Content-Length'] = len(response.content)
-        if is_compressed:
-            response['Content-Encoding'] = 'gzip'
-            
-        return response
-    
-    except Exception as e:
-        return _error_response(e)
+    # NOTE: caching is not employed for package list
+              
+    is_compressed = False
+    if extension == constants.GZIP_EXTENSION:
+        is_compressed = True
+    elif extension != '':
+        return HttpResponse(status_code=404)
         
-
+    mimetype = 'text/plain'
+    if is_compressed:
+        mimetype = 'application/gzip'
+    repository = get_repository_controller(request=request)
+    response = HttpResponse(mimetype=mimetype)
+    response.content = repository.get_packages(distribution, section, architecture,
+                                               is_compressed)
+    response['Content-Length'] = len(response.content)
+    if is_compressed:
+        response['Content-Encoding'] = 'gzip'
+        
+    return response
+        
+@handle_exception
 def release_list(request, distribution, extension):
     """
     Retrieves a Releases metafile list
     """
-    try:
-        if request.method != 'GET':
-            raise AptRepoException('Invalid HTTP method')
+    if request.method != 'GET':
+        raise BadHTTPMethodException(request)
 
-        repository = get_repository_controller(request=request)
-        (releases_data, signature_data) = repository.get_release_data(distribution)
-        data = None
-        if extension == constants.GPG_EXTENSION:
-            data = signature_data
-        else:
-            data = releases_data
+    repository = get_repository_controller(request=request)
+    (releases_data, signature_data) = repository.get_release_data(distribution)
+    data = None
+    if extension == constants.GPG_EXTENSION:
+        data = signature_data
+    else:
+        data = releases_data
 
-        # return the response
-        return HttpResponse(data, mimetype = 'text/plain')
+    # return the response
+    return HttpResponse(data, mimetype = 'text/plain')
 
-    except Exception as e:
-        return _error_response(e)
 
 def _handle_uploaded_file(request, distribution_name, section_name, uploaded_file):
     """ 
@@ -210,14 +234,3 @@ def _handle_remove_package(request, package_instance_id):
     repository = get_repository_controller(request=request)
     repository.remove_package(package_instance_id)
     return HttpResponseRedirect(reverse(remove_success))
-
-def _error_response(exception):
-    """ 
-    Error response 
-    """
-    if settings.DEBUG:
-        raise exception
-    else:
-        logger = logging.getLogger(settings.DEFAULT_LOGGER)
-        logger.exception(exception)
-        return HttpResponse(content=exception.__str__(), status=406)
